1bd02e331c22fcc85ab62cfc068ab8a193733b4d
“Aurelie” <aurelie_boutin1@baylor.edu>
2022-03-16 13:34:20 -0500

    Finished Series

--- pandas1.py
+++ pandas1.py
@@ -1,3 +1,4 @@
+from readline import get_history_item
 import pandas as pd
 
 grades = pd.Series([87,100,94])
@@ -29,6 +30,44 @@ print(grades_ds)
 
 eva = grades['Eva']
 wally = grades.Wally
+
+e = grades.values
+
+hardware = pd.Series(['Hammer','Saw','Wrench'])
+f = hardware.str.contains('a')
+g = hardware.str.upper()
+
+#convert a series object to a Python list
+hardware_list = hardware.to_list()
+
+ds1 = pd.Series([2,4,6,8,10])
+ds2 = pd.Series([1,3,5,7,9])
+
+g = ds1 == ds2
+h = ds1 > ds2
+i = ds1 < ds2
+#print(g)
+#print(h)
+#print(i)
+
+list_of_series = pd.Series([['Red','Green','White'], ['Red','Black'], ['Yellow']])
+list_of_series = list_of_series.apply(pd.Series).stack().reset_index(drop=True) # making just one series and resetting all the indexes
+
+# sort a series
+s = pd.Series(['100','200','python','300.12','400'])
+sorted_series = s.sort_values() # can do sort index or sort values, we may want to sort based on values because index is one or zero
+
+#s = pd.Series(['100',200,'python',300.12,'400']) # error because integers and float, it can't sort elements that have different data types
+#sorted_series = s.sort_values() 
+
+#adding to a Series
+s = s.append(pd.Series(['500','php'])).reset_index(drop=True) # have to reassign it to s
+
+# write a Pandas program to calculate the frequency counts of each unique value of a given series.
+import random
+list1 = [random.randrange(1,10) for i in range(0,100)]
+s = pd.Series(list1)
+result = s.value_counts()
 
 print()
 
---
+++ pandas_Df.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+grades_dict = {'Wally':[87,96,70],'Eva':[100,87,90],'Sam':[94,77,90],'Katie':[100,81,82],'Bob':[83,65,85]}
+
+# a series is one-dimensional array
+# a data frame is a two-diensional array
+# each column on a data frame is a series
+
